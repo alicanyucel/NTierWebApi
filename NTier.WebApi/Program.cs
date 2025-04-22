fc55c0bc6781fcd43e5221c42376f5cb3a29b807
Ali Can Yücel (The Coderman) <yucelalican@hotmail.com>
2025-04-22 12:36:41 +0300

    cors policy

--- NTier.WebApi/Program.cs
+++ NTier.WebApi/Program.cs
@@ -2,24 +2,30 @@ using Microsoft.EntityFrameworkCore;
 using NTier.Repositories.EFCore.Context;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddSwaggerGen();
 builder.Services.AddControllers();
 builder.Services.AddDbContext<RepositoryContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("SqlServer"));
-}); 
+});
+builder.Services.AddSwaggerGen();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", builder =>
+    {
+        builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
+    });
+});
+
 var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
-    
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
+app.UseRouting();
+app.UseCors("AllowAll");
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
